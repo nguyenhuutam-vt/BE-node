3b390c354d118ec674c2ac5fa43da05bc5795b1f
nguyenhuutam-vt <nguyenvinhspkt10@gmail.com>
2023-06-14 13:44:11 +0700

    blank q4

--- src/index.js
+++ src/index.js
@@ -48,6 +48,8 @@
 const express = require('express')
 const app = express()
 const port = 8000
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/',(req,res)=>{
     res.send('HomeWork')
--- src/router/output.js
+++ src/router/output.js
@@ -1,8 +1,49 @@
 const express = require('express')
 const router = express.Router();
+const fs = require('fs');
 const outputController = require('../controller/output')
-router.get('/outputs',outputController.getAllBook);
+// router.get('/outputs',outputController.getAllBook);
 
-router.post('/createUser',outputController.createOutput)
+router.get('/getAll',(req,res)=>{
+    fs.readFile("output.json",(err,data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send("Error reading data from file");
+        }
+        else{
+            const jsonData = JSON.parse(data);
+            res.send(jsonData);
+        }
+    })
+})
+
+// router.get('/getDataByCountry/:country', (req, res) => {
+//     const country = req.params.country;
+
+//     fs.readFile("output.json", (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send("Error reading data from file");
+//         } else {
+//             const jsonData = JSON.parse(data);
+//             const filteredData = jsonData.filter((item) => item.country === country);
+//             res.send(filteredData);
+//         }
+//     });
+// });
+
+router.post('/createUser',(req,res)=>{
+    const data = JSON.stringify(req.body);
+    fs.writeFile("output.json", data, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error writing data to file");
+        } else {
+            res.send("Data written to file successfully");
+        }
+    });
+})
+
+// router.post('/createUser',outputController.createOutput)
 
 module.exports = router;
